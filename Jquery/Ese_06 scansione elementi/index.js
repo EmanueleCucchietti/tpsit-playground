a6a05d8bf49e768c5ab9bd7b5dde35eba4869f0e
Stud. Emanuele Cucchietti <e.cucchietti.1732@vallauri.net>
2021-11-02 11:14:32 +0100

    Ex 6 scansione elementi-1

--- Jquery/Ese_06 scansione elementi/index.js
+++ Jquery/Ese_06 scansione elementi/index.js
@@ -10,18 +10,35 @@ $(document).ready(function(){
 	wrapper = $("#wrapper");
 
 	$("#btn1").on("click",function(){
-		alert(wrapper.children().lenght);
+		alert(wrapper.children().length);
 	})
 	
 	$("#btn2").on("click",function(){
-		alert("not implemented");
-		/*
 		let str = "";
-		for(let i=0;i<wrapper.children().lenght; i++)
-			str += " " + wrapper.children().text();
+		/*
+		for(let i=0;i<wrapper.children().length; i++)
+			str += " " + wrapper.children().eq(i).text() + "\n";
+			*/
+		for (const [i,item] of wrapper.children().toArray().entries())
+			str += " " + $(item).text() + "\n";
+		alert(str);
 
-		*/
 	})
 
+	$("#btn3").on("click",function(){
+		wrapper.children().css("backgroundColor","");
+		wrapper.children(":nth-of-type(even)").css("backgroundColor","yellow");
+	})
 
+	$("#btn4").on("click",function(){
+		wrapper.children(":nth-of-type(odd)").toArray().forEach((e,i) => {
+			console.log(e)
+			let color = "rgb(0,"+50*(i+1)+", 0";
+			$(e).css("color",color);
+		})
+		wrapper.children(":nth-of-type(odd)").each(function(i,ref){
+			let color = "rgb(0,"+50*(i+1)+", 0";
+			//$(ref).css("color",color);
+		})
+	})
 })
