b1ddc75785a66658b70cd78c8e57d791d033db55
Stud. Emanuele Cucchietti <e.cucchietti.1732@vallauri.net>
2021-11-18 08:44:32 +0100

    Ex 13 Forza Quattro-1

--- Jquery/ese_13 ForzaQuattro/index.js
+++ Jquery/ese_13 ForzaQuattro/index.js
@@ -29,12 +29,12 @@ $(document).ready(function(){
         for(let i=0;i<COLONNE;i++){
             let div = creaBox(header);
             div.on("mouseenter",function(){
-                $(this).css("background-color", GIALLO);
+                $(this).css("background-color", turno);
             })
             div.on("mouseleave",function(){
                 $(this).css("background-color", GRIGIO);
             })
-            div.on("click",function(){
+            div.on("click",function discesa(){
                 let colonna = header.children().index($(this));
                 let j=0;console.log(wrapper.children('#btn-'+j+'-'+colonna).css("background-color"))
                 while(wrapper.children('#btn-'+j+'-'+colonna).css("background-color")==GRIGIO && j<RIGHE){
@@ -42,11 +42,22 @@ $(document).ready(function(){
                     j++;
                 }
                 j--;
-                if(j==0)
+                if(j==-1)
                     alert("Mossa non valida");
                 else    
                     {
-                        let newPedina = $("<div>").addClass("pedina").appendTo(wrapper).css({'position': 'absolute','background-color': turno, 'top': -60, 'left' : colonna *60}).animate({'top': j*60+5});
+                        let color = turno
+                        if(turno == GIALLO)
+                            turno = ROSSO
+                        else
+                            turno = GIALLO
+                        header.children().off("click");
+                        let newPedina = $("<div>").addClass("pedina").appendTo(wrapper).css({'position': 'absolute','background-color': turno, 'top': -60, 'left' : colonna *60+5})
+                        .animate({'top': j*60+5},200*(j+1),e=>{
+                            $('#btn-'+j+'-'+colonna).css("background-color",color);
+                            header.children().on("click",discesa);
+                            $(this).css("background-color", color);
+                        });
                     }
             })
         }
