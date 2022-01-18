0cf63da31d70405b0ec1c648781f1e0f44798c55
EmanueleCucchietti <e.cucchietti.1732@vallauri.edu>
2022-01-18 11:36:18 +0100

    Ex 03 Quadri-1

--- Ajax/ese 03 Quadri/index.js
+++ Ajax/ese 03 Quadri/index.js
@@ -5,6 +5,8 @@ const URL = "http://localhost:3000"
 let artisti;
 let quadri;
 let artistaSelezionato;
+let index = 0;
+
 
 $(function () {
     let _header = $('.header');
@@ -22,10 +24,10 @@ $(function () {
         artisti = data;
         for (const item of artisti) {
             let label = $("<label>").appendTo(_header);
-            let radio = $("<input>").prop("type", "radio").prop("name", "rdbArtista").on("change", caricaQuadri).prop("artista",item).appendTo(label);
+            let radio = $("<input>").prop("type", "radio").prop("name", "rdbArtista").on("change", caricaQuadri).prop("artista", item).appendTo(label);
             label.append(item["name"])
         }
-        _header.find("input[type=radio]").eq(generaNumero(0, artisti.length-1)).prop("checked", true);
+        _header.find("input[type=radio]").eq(generaNumero(0, artisti.length - 1)).prop("checked", true);
         caricaQuadri()
 
     })
@@ -33,21 +35,12 @@ $(function () {
     function caricaQuadri() {
         let artista = _header.find("input[type=radio]:checked").prop("artista");
         let idArtista = artista.id;
-        let request = inviaRichiesta("GET", URL + "/quadri", {"artist": idArtista});
+        let request = inviaRichiesta("GET", URL + "/quadri", { "artist": idArtista });
         request.fail(errore);
-        request.done(function(data){
-            quadri=data;
-            _info.html("");
-            $("<div>").html("ID = <b>" + quadri[0].id + "</b>").appendTo(_info)
-            $("<div>").html("Titolo = <b>" + quadri[0].title + "</b>").appendTo(_info)
-            $("<div>").html("Genere = <b>" + quadri[0].id + "</b>").appendTo(_info)
-            let img = $("<img>").prop("src", "img/like.jpg");
-            img.css({"width": "30px", "vertical-align": "middle", "margin-left": "15px"});
-            img.on("click", incrementaLike)
-
-            $("<div>").html("Like = <b>" + quadri[0].nLike + "</b>").append(img).appendTo(_info)
-            _img.css("background-image", "url('img/"+ quadri[0].img + "')")
-            _img.css("width","200px").css("height","150px").css("background-size","cover")
+        request.done(function (data) {
+            index = 0;
+            quadri = data;
+            visualizzaQuadro();
         })
 
         /*
@@ -59,8 +52,61 @@ $(function () {
             
         })*/
     }
-    function incrementaLike(){
-        console.log("aumento")
+    function incrementaLike() {
+        let like = $(this).prop("nLike");
+        let thisOne = this
+        $(this).hide();
+        like++;
+        let request = inviaRichiesta("PATCH", URL + "/quadri/" + quadri[index].id, { "nLike": like })
+        request.fail(errore)
+        request.done((data) => {
+            $(thisOne).prop("nLike", like);
+            $(thisOne).parent().html("Like = <b> " + like + "</b>");
+        })
+    }
+    function next() {
+        index++;
+        visualizzaQuadro()
+    }
+    function prev() {
+        index--;
+        visualizzaQuadro()
+    }
+    function visualizzaQuadro() {
+        let artista = _header.find("input[type=radio]:checked").prop("artista");
+        if (index == 0)
+            _btnPrev.prop("disabled", true);
+        else
+            _btnPrev.prop("disabled", false);
+
+        if (index == quadri.length - 1)
+            _btnNext.prop("disabled", true);
+        else
+            _btnPrev.prop("disabled", false)
+
+        _info.empty();
+
+        $("<div>").html("ID = <b>" + quadri[index].id + "</b>").appendTo(_info)
+        $("<div>").html("Titolo = <b>" + quadri[index].title + "</b>").appendTo(_info)
+        $("<div>").html("Genere = <b>" + quadri[index].id + "</b>").appendTo(_info)
+        let img = $("<img>").prop("src", "img/like.jpg");
+        img.prop("nLike", quadri[index].nLike)
+        img.css({ "width": "30px", "vertical-align": "middle", "margin-left": "15px" });
+        img.on("click", incrementaLike)
+
+        $("<div>").html("Like = <b>" + quadri[index].nLike + "</b>").append(img).appendTo(_info)
+        _img.css("background-image", "url('img/" + quadri[index].img + "')")
+        _img.css("width", "200px").css("height", "150px").css("background-size", "cover")
     }
 
+    _btnNext.on("click", next)
+    _btnPrev.on("click", prev)
+    $("#btnSalva").on("click", function () {
+        let file = $("#txtFile").prop("files")[0];
+        let reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onloadend = function () {
+            _img.prop("src", reader.result)
+        }
+    })
 })
