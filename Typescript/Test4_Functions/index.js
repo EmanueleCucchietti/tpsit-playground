84823660879691fa78b0184a5f04cd6740e8357a
EmanueleCucchietti <e.cucchietti.1732@vallauri.edu>
2022-03-01 14:32:17 +0100

    Test4:Functions

---
+++ Typescript/Test4_Functions/index.js
@@ -0,0 +1,8 @@
+function printPow(x, y) {
+    console.log(Math.pow(x, y).toString());
+}
+function getPow(x, y) {
+    console.log(Math.pow(x, y).toString());
+}
+printPow(5, 3);
+let x = getPow(3, 4);
