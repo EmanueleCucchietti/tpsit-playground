de649495331633e6fe234334b27d090a0a1b079f
EmanueleCucchietti <e.cucchietti.1732@vallauri.edu>
2022-03-01 13:55:08 +0100

    TypeScript Test1:BasicTypes

---
+++ Typescript/Test1/index.js
@@ -0,0 +1,10 @@
+let lucky = 23; //type-> number
+let notLucky = 2; //type-> any
+let maybeLucky; //type-> any
+//not assignable to a string or any other types other than number
+lucky = 2;
+//assignable to any types
+notLucky = "3";
+async function hello() {
+    return "World";
+}
