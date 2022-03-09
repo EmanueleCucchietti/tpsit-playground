e9024da82c531e265fde790c53829eca51ce0062
EmanueleCucchietti <e.cucchietti.1732@vallauri.edu>
2022-03-09 11:41:48 +0100

    Es05-06 Update

--- Geolocation/Es 05 percorso/index.js
+++ Geolocation/Es 05 percorso/index.js
@@ -3,7 +3,7 @@
 const URL = "https://maps.googleapis.com/maps/api"
 
 window.onload = async function () {
-// $(async function(){ jqueryOnloadNotWorking
+	// $(async function(){ jqueryOnloadNotWorking
 
 	// creazione dinamica del CDN di accesso alle google maps
 	//per non mettere la chiave dentro HTML
@@ -20,78 +20,79 @@ window.onload = async function () {
 		loadMap();
 	})
 
-	function loadMap(){let partenza = $('input[type=text]').eq(0).val();
-	let arrivo = header.find('input').eq(1).val();
+	function loadMap() {
+		let partenza = $('input[type=text]').eq(0).val();
+		let arrivo = header.find('input').eq(1).val();
 
-	let directionsService = new google.maps.DirectionsService();
-	let routesOptions = {
-		origin: partenza,
-		destination: arrivo,
-		travelMode: google.maps.TravelMode.DRIVING, // default
-		provideRouteAlternatives: true, // default=false
-		avoidTolls: false, // default (pedaggi)
-	};
-
-
-
-	//Soluzione con promise --> senza callBack
-	let promise = directionsService.route(routesOptions)
-	promise.then(function (directionsRoutes) {
-		let mapOptions = {
-			/*'zoom': 15,
-			'mapTypeId': google.maps.MapTypeId.ROADMAP*/
+		let directionsService = new google.maps.DirectionsService();
+		let routesOptions = {
+			origin: partenza,
+			destination: arrivo,
+			//travelMode: google.maps.TravelMode.DRIVING, // default
+			provideRouteAlternatives: true, // default=false
+			//avoidTolls: false, // default (pedaggi)
 		};
-		let mappa = new google.maps.Map(divMap, mapOptions);
-		if (directionsRoutes.status == google.maps.DirectionsStatus.OK) {
-			console.log(directionsRoutes.routes[0])
-			let i = 1;
-
-			for (const route of directionsRoutes.routes) {
-				let color;
 
 
-				if (i == 1) {
-					color = "#44F";
-				}
-				else {
-					color = `rgb(${60 * i}, ${60 * i}, ${60 * i})`
-				}
-				let renderOptions = {
-					polylineOptions: {
-						strokeColor: color, // colore del percorso
-						strokeWeight: 6, // spessore
-						zIndex: 100 - i // posizionamento
+
+		//Soluzione con promise --> senza callBack
+		let promise = directionsService.route(routesOptions)
+		promise.then(function (directionsRoutes) {
+			let mapOptions = {
+				/*'zoom': 15,
+				'mapTypeId': google.maps.MapTypeId.ROADMAP*/
+			};
+			let mappa = new google.maps.Map(divMap, mapOptions);
+			if (directionsRoutes.status == google.maps.DirectionsStatus.OK) {
+				console.log(directionsRoutes.routes[0])
+				let i = 1;
+
+				for (const route of directionsRoutes.routes) {
+					let color;
+
+
+					if (i == 1) {
+						color = "#44F";
 					}
-				}
+					else {
+						color = `rgb(${60 * i}, ${60 * i}, ${60 * i})`
+					}
+					let renderOptions = {
+						polylineOptions: {
+							strokeColor: color, // colore del percorso
+							strokeWeight: 6, // spessore
+							zIndex: 100 - i // posizionamento
+						}
+					}
 
-				let directionRenderer = new google.maps.DirectionsRenderer(renderOptions);
+					let directionRenderer = new google.maps.DirectionsRenderer(renderOptions);
 
-				directionRenderer.setMap(mappa);
-				directionRenderer.setRouteIndex(i - 1);
-				directionRenderer.setDirections(directionsRoutes);
+					directionRenderer.setMap(mappa);
+					directionRenderer.setRouteIndex(i - 1);
+					directionRenderer.setDirections(directionsRoutes);
 
 
-				//prima di scrivere nel panel bisogna sempre cancellare prima il contenuto
-				if (i == 1) {
-					panel.innerHTML = "";
-					directionRenderer.setPanel(panel);
-				}
+					//prima di scrivere nel panel bisogna sempre cancellare prima il contenuto
+					if (i == 1) {
+						panel.innerHTML = "";
+						directionRenderer.setPanel(panel);
+					}
 
-				let distanza = route.legs[0].distance.text;
-				let tempo = route.legs[0].duration.text;
-				msg.html(msg.html() + `<b>Percorso ${i}: </b>Distanza: <b>${distanza}</b>
+					let distanza = route.legs[0].distance.text;
+					let tempo = route.legs[0].duration.text;
+					msg.html(msg.html() + `<b>Percorso ${i}: </b>Distanza: <b>${distanza}</b>
 			Tempo di percorrenza: <b>${tempo}</b><br>`);
 
-				//perparo i colori per la route successiva
+					//perparo i colori per la route successiva
 
-				i++;
+					i++;
+				}
+
 			}
-
-		}
-	});
-	promise.catch(function () {
-		console.log('errore');
-	})
+		});
+		promise.catch(function () {
+			console.log('errore');
+		})
 
 	//Soluzione senza promise --> con callBack
 	/*directionsService.route(routesOptions, function(directionsRoutes){
