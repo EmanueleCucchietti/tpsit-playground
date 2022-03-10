5342f589be13f7581f8ab577a0677d1783f761f6
EmanueleCucchietti <e.cucchietti.1732@vallauri.edu>
2022-03-10 08:12:05 +0100

    Ex Verifica

---
+++ Geolocation/Verifica/libreria.js
@@ -0,0 +1,20 @@
+function loadGoogleApi() {
+	return new Promise((resolve, reject) => {
+		var script = document.createElement('script');
+		document.body.appendChild(script);
+		//img.src = src
+		script.type = 'text/javascript';
+		script.src = URL + '/js?v=3&key=' + MAP_KEY;
+		script.onload = () => resolve()
+		script.onerror = reject
+	})
+}
+function generaColore() {
+	let R = generaNumero(0, 255);
+	let G = generaNumero(0, 255);
+	let B = generaNumero(0, 255);
+	return (`rgba(${R}, ${G}, ${B}, 0.8)`);
+}
+function generaNumero(a, b){
+	return Math.floor((b-a+1)*Math.random()) + a;
+}
